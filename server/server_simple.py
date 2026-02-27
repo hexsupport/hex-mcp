e68c0ed8fcfd100249169dbc2e59f35198db16ff
mizall90 <mizall90@gmail.com>
2026-02-27 17:54:04 +0545

    disable usecase, model, modelcard tools

--- server/server_simple.py
+++ server/server_simple.py
@@ -9,9 +9,9 @@ from config import mcp, config
 from clients import server_lifespan
 
 # Import all tool modules to register their MCP tools
-import usecase_tools
-import model_tools
-import modelcard_tools
+# import usecase_tools
+# import model_tools
+# import modelcard_tools
 import forecasting_tools
 
 # The server is ready - fastmcp will handle the rest
